510677decdfc3bfb47b7d21d26f4d5da2ebaeec4
Casey Schneider-Mizell <caseysm@gmail.com>
2018-09-03 13:26:55 -0700

    small

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.1',
     author='Casey Schneider-Mizell',
     author_email='caseysm@gmail.com',
+    description='Package to get bibtex from various reference ids',
     packages=setuptools.find_packages()
     )
