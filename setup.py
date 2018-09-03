dbf6b0c3a88063ddb5c06f4e6166fb2996e0553d
Casey Schneider-Mizell <caseysm@gmail.com>
2018-09-03 13:26:43 -0700

    Added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='refid2bib',
+    version='0.1',
+    author='Casey Schneider-Mizell',
+    author_email='caseysm@gmail.com',
+    packages=setuptools.find_packages()
+    )
