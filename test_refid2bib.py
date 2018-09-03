9f60ca083832db309e2a796e9612f86b3d290c31
Casey Schneider-Mizell <caseysm@gmail.com>
2018-09-03 13:48:22 -0700

    Tweaked test examples

--- test_refid2bib.py
+++ test_refid2bib.py
@@ -1,16 +1,16 @@
-from refid2bib import *
+from refid2bib import refid2bib
 
 doi_biorxiv = "https://doi.org/10.1101/406314"
-doi_arxiv = "arXiv:1801.04381"
+arxiv_id = "arXiv:1801.04381"
 doi_general = "DOI: 10.1126/science.1260088"
 pmid = 'PMID: 25056931'
 pmc_id = 'PMC3711719'
 
-print( 'General example:')
+print( 'DOI example:')
 print( refid2bib(doi_general) )
 
 print( 'Arxiv example:')
-print( refid2bib(doi_arxiv) )
+print( refid2bib(arxiv_id) )
 
 print( 'Biorxiv example:')
 print( refid2bib(doi_biorxiv) )
@@ -21,13 +21,11 @@ print( refid2bib(pmid) )
 print( 'PMC example:')
 print( refid2bib(pmc_id) )
 
-
-print( 'Specified example:')
+print( 'Doi example, specified reference type:')
 print( refid2bib(doi_general, ref_type='doi') )
 
+print( 'Arxiv example, custom short name:')
+print( refid2bib(arxiv_id, short_name='my_custom_name' ) )
 
-print( 'Arxiv example custom short name:')
-print( refid2bib(doi_general, short_name='my_custom_name' ) )
-
-print( 'General example switched name order:')
+print( 'General example, switched name order:')
 print( refid2bib(doi_general, lastname_first=False ) )
